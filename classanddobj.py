2950f2c0d394765082183d53545659d595fc368a
shivanshxpandey24 <shivanshxpandey24@gmail.com>
2026-05-22 00:57:04 +0530

    Update classanddobj.py

--- classanddobj.py
+++ classanddobj.py
@@ -1,40 +1,40 @@
-# class SharmaVishnu:
-#     a = 'lolo'     # CLASS KE ANDAR KE VARIABLES ARE CALLED ATTRIBUTES
+class SharmaVishnu:
+    a = 'lolo'     # CLASS KE ANDAR KE VARIABLES ARE CALLED ATTRIBUTES
     
-#     def sample():  # CLASS KE ANDAR KE FUNCTION ARE CALLED METHODS
-#         print('This is a Sample Function')
+    def sample():  # CLASS KE ANDAR KE FUNCTION ARE CALLED METHODS
+        print('This is a Sample Function')
     
-# SharmaVishnu.sample()
-# print(SharmaVishnu.a)
+SharmaVishnu.sample()
+print(SharmaVishnu.a)
 
-# class Animals:
-#     name = "Animal"
+class Animals:
+    name = "Animal"
     
-#     def greet(self):     
-#         # JAb bhi class ke ANDAR KE FUNCTION KO OBJECT KI HELP SE CALL KARENGE TO USSEY EK PARAMETERE DENA HOGA
-#         print('This is Animal Class')
+    def greet(self):     
+        # JAb bhi class ke ANDAR KE FUNCTION KO OBJECT KI HELP SE CALL KARENGE TO USSEY EK PARAMETERE DENA HOGA
+        print('This is Animal Class')
         
-# tau = Animals()  # Here Tau is Object --> Class Stored in variable is called Object
-# mama = Animals()
-# mama.greet()
+tau = Animals()  # Here Tau is Object --> Class Stored in variable is called Object
+mama = Animals()
+mama.greet()
 
 
-# # CREATE A TASK WHICH WILL PERFORM TWO TASKS 
-# # 1. Greet the user - "This is______class "
-# # 2. Addind up two no.
+# CREATE A TASK WHICH WILL PERFORM TWO TASKS 
+# 1. Greet the user - "This is______class "
+# 2. Addind up two no.
 
-# class Add:
-#     def greet(self):
-#         print(" hello from shivansh")
+class Add:
+    def greet(self):
+        print(" hello from shivansh")
         
-#     def add(self):
-#         a=10
-#         b=20
-#         print(a+b)
+    def add(self):
+        a=10
+        b=20
+        print(a+b)
         
-# obj = Add()
-# obj.greet()
-# obj.add()
+obj = Add()
+obj.greet()
+obj.add()
         
         
 # CLASS METHOD --> this can help on changing the class attributes 
